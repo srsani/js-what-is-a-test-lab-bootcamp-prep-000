452b566454bff4bd20711e150b6d5284eafb57de
droll-accelerator-1483 <srsani@gmail.com>
2019-06-19 10:18:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
-var name = "Joe"
-var height = 74
+var name = "Sussan"
+var height = 36
 var message = ` is ${height} inches tall`
 
 module.exports = {name, height, message}
